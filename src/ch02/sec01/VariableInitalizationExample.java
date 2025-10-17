cba4c9a59556312a040034ee95882db7d6661f79
whitedog0980opened <whitedog0980opened@gmail.com>
2025-10-17 13:53:26 +0000

    변수 선언 예시

---
+++ src/ch02/sec01/VariableInitalizationExample.java
@@ -0,0 +1,9 @@
+package ch02.sec01;
+
+public class VariableInitalizationExample {
+    public static void main(String[] args) {
+        int value; // 변수 선언
+        value = 10; // 변수 초기화
+        System.out.println("value: " + value); // 초기화된 변수 사용
+    }
+}
