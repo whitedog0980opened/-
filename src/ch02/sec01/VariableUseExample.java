68bb78ce40e3d9a781e59681612d31193448140d
whitedog0980opened <whitedog0980opened@gmail.com>
2025-10-17 13:55:51 +0000

    3

---
+++ src/ch02/sec01/VariableUseExample.java
@@ -0,0 +1,10 @@
+package ch02.sec01;
+
+public class VariableUseExample {
+    public static void main(String[] args) {
+        int number; // 변수 선언
+        number = 5; // 변수 초기화
+        int result = number * 2; // 변수 사용
+        System.out.println("Result: " + result);
+    }
+}
